276eb55b606d6cbb312eadda66889e2b855a26dc
ws303 <w.see@exeter.ac.uk>
2021-01-31 15:47:25 +0000

    Victor's update on title and blurb

--- js/main.js
+++ js/main.js
@@ -27,3 +27,14 @@ document.addEventListener("DOMContentLoaded", function(){
     let readmores = document.querySelectorAll(".readmore")
     for (const rm of readmores) { rm.addEventListener('click', showMore, false) }
 })
+
+window.onload = function writeBlurb() {
+	let cards = document.querySelectorAll(".card-wrapper")
+	
+	str1 = "Welcome to our digital wall of anonymous letters. These are letters that have been submitted by participants of the 2021 European Astronomical Society Annual Meeting as part of the special session on welfare and mental health in astronomy (SS8). They detail the lived experiences of those in astronomical research. The submissions have been been screen by the session SOC to ensure they are not racist, homophobic, transphobic, sexist, ableist, etc. and that they do not disclose any identifying details. So far we have received "
+	str2 = str1.concat(cards.length.toString())
+	str3 = str2.concat(" letters. Below, there are a number of tags that you can select/deselect if you wish to see/exclude specific topics. For more information, please see the <a href='https://eas.unige.ch/EAS_meeting/session.jsp?id=SS8'>session website</a>.")
+	
+	document.getElementById("blurb").innerHTML = str3
+}
+
