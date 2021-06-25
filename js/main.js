3a3fc70d45ebc16f56858868001ca813fd67b55c
Jost Migenda <jost.migenda@kcl.ac.uk>
2021-06-25 19:44:06 +0200

    normalize tags better

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ function filterCards() {
         card.style.display = ""
         let currentTags = card.querySelector(".tags").textContent.split("; ")
         for (const tag of currentTags) {
-            const spaceless_tag = tag.replaceAll(' ', '')
-            if (showTag[spaceless_tag] == false) {
+            const normalized_tag = tag.replaceAll(' ', '').toLowerCase().replaceAll(';', '')
+            if (showTag[normalized_tag] == false) {
                 card.style.display = "none"
             }
         }
